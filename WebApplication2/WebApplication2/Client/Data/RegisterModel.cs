192599a080721e26b0525c83efba2e37a5adf1cd
Hamzah Mahmood <Hamzah@Mahmood.com>
2021-08-25 18:33:29 +0500

    My First Blazor Webassembly App

---
+++ WebApplication2/WebApplication2/Client/Data/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Client.Data
+{
+    public class RegisterModel
+    {
+        public string fullName;
+        public string email;
+        public string password;
+        public string confirmPassword;
+    }
+}
